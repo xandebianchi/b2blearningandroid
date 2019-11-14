91c0dc096d56d463f3e43d83cb2cbf4fe8dece2d
LuisFernandoSFonseca <43556626+LuisFernandoSFonseca@users.noreply.github.com>
2019-11-14 09:16:58 -0300

    Login layout (#6)
    * Login Layout created, pushing to git for code review
    
    * resolving conflicts

--- app/src/main/java/com/example/b2blearning/MainActivity.java
+++ app/src/main/java/com/example/b2blearning/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.b2blearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = (Button)findViewById(R.id.loginBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
