5155089531e7b46551ff76821c9fe7b7ab477e55
LuisFernandoSFonseca <luisf_sanches@live.com>
2019-11-27 11:35:29 -0300

    Card list layout and writable quantity button

--- app/src/main/java/com/example/b2blearning/Beer.java
+++ app/src/main/java/com/example/b2blearning/Beer.java
@@ -4,12 +4,14 @@ public class Beer {
     private String title;
     private String description;
     private String price;
+    private String quantity;
     private String nameOfImage;
 
     public Beer (String title, String description, String price){
         this.title = title;
         this.description = description;
         this.price = price;
+        this.quantity = quantity;
         this.nameOfImage = nameOfImage;
     }
 
@@ -23,6 +25,14 @@ public class Beer {
 
     public String getPrice() {
         return price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
     }
 
     public String getNameOfImage() {
